26395e6523ea1f3db59d3f33b216a1c2d1ea77ea
Maki Time <lixenrylie@gmail.com>
2025-09-21 14:09:38 +1000

    Changed some stuff
    package.json
    next.config.ts
    PromptInput.tsx

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
